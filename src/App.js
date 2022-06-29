53d612feeabf44da3fec0de8b50939deb3fe2adb
Josh <alejandromzv18@gmail.com>
2022-06-28 22:05:36 -0400

    to deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Formik, Form, Field } from 'formik'
+import './header.css'
+import './container.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+function App(){
+
+  const [photos, setPhotos] = useState([])
+  const open = link => window.open(link)
+
+  return(
+    <div>
+      <header>
+        <Formik
+        initialValues={{search: ''}}
+        onSubmit={ async values => {
+
+          const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+          {
+            headers:{
+              'Authorization': 'Client-ID e4yqBOKmw8zOS1a4lPSRcDGykzf7-ngQ6W5PNENbTPo'
+            }
+          })
+
+          const data = await response.json()
+          setPhotos(data.results)
+        } 
+      }
         >
-          Learn React
-        </a>
+           <Form>
+             <Field name="search" type="text" />
+             <button type="submit">Search</button>
+           </Form>
+
+        </Formik>
       </header>
+      <div className="container">
+          <div className="center">
+            {photos.map(photo => 
+              <article key={photo.id} onClick={() => open(photo.links.html)}>
+                <img src={photo.urls.regular} alt={photo.alt_description}/>
+                <p>{[photo.description, photo.alt_description].join(' - ')}</p>
+              </article>
+            )}
+          </div>
+        </div>
     </div>
-  );
+    )
+
 }
 
 export default App;
